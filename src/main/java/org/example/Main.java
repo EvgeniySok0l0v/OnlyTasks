8e045eec2384ee2e21af20726a97cbc3d604c91a
Yauheni Sokalau <koten0k335a@gmail.com>
2024-04-14 19:05:44 +0300

    factorial method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,32 @@
 package org.example;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        System.out.println(factorial(300));
+
+    }
+
+    public static BigInteger factorial(Number n){
+        long intValue = n.longValue();
+        if(intValue < 0){
+            throw new IllegalArgumentException("Отрицательное число");
+        }
+        if(intValue == 0 || intValue == 1){
+            return BigInteger.ONE;
+        }
+        BigInteger f = BigInteger.ONE;
+
+        while (intValue != 0){
+            f = f.multiply(BigInteger.valueOf(intValue--));
+        }
+
+        return f;
     }
 }
